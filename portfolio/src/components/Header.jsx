5921e3a2b6595f87bab35a5602001790e1445475
Ross Matthew Santos <matthewsantos120216@gmail.com>
2024-02-22 16:36:01 +0800

    added onboard animation for the stacks icons

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -26,26 +26,38 @@ export default function Header() {
           </div>
         </div>
         <div className='flex items-center gap-10 text-white'>
-            <MagneticIcon className='bg-[#214062] h-28 w-28 flex justify-center items-center rounded-full shadow-2xl' data-aos='zoom-in'>
-              <img src={sassLogo} className='size-14'/>
+            <MagneticIcon >
+              <div className='bg-[#214062] h-28 w-28 flex justify-center items-center rounded-full shadow-2xl' data-aos='zoom-in' data-aos-duration="1000" data-aos-once="true" data-aos-delay="1200">
+                <img src={sassLogo} className='size-14'/>
+              </div>
             </MagneticIcon>
-            <MagneticIcon className='bg-[#214062] h-32 w-32 flex justify-center items-center rounded-full shadow-2xl'>
-              <img src={tsLogo} className='size-16'/>
+            <MagneticIcon>
+              <div className='bg-[#214062] h-32 w-32 flex justify-center items-center rounded-full shadow-2xl' data-aos='zoom-in' data-aos-duration="1000" data-aos-once="true" data-aos-delay="1000">
+                <img src={tsLogo} className='size-16'/>
+              </div>
             </MagneticIcon>
-            <MagneticIcon className='bg-[#214062] h-40 w-40 flex justify-center items-center rounded-full shadow-2xl'>
-              <img src={reactLogo} className='size-24'/>
+            <MagneticIcon>
+              <div className='bg-[#214062] h-40 w-40 flex justify-center items-center rounded-full shadow-2xl'  data-aos='zoom-in' data-aos-duration="1000" data-aos-once="true" data-aos-delay="800">
+               <img src={reactLogo} className='size-24'/>
+              </div>
             </MagneticIcon>
-            <div className='-z-50 relative px-8 bg-[#214062] w-[280px] rounded-2xl h-[420px] shadow-2xl'>
+            <div className='-z-50 relative px-8 bg-[#214062] w-[280px] rounded-2xl h-[420px] shadow-2xl' data-aos='zoom-in' data-aos-duration="1000" data-aos-once="true" data-aos-delay="400">
                 <img src={img} className='absolute -top-[60px] right-0 h-[480px] drop-shadow-2xl'/>
             </div>
-            <MagneticIcon className='bg-[#214062] h-40 w-40 flex justify-center items-center rounded-full shadow-2xl'>
-              <img src={nextLogo} className='size-28'/>
+            <MagneticIcon>
+              <div  className='bg-[#214062] h-40 w-40 flex justify-center items-center rounded-full shadow-2xl' data-aos='zoom-in' data-aos-duration="1000" data-aos-once="true" data-aos-delay="800">
+                <img src={nextLogo} className='size-28'/>
+              </div>
             </MagneticIcon>
-            <MagneticIcon className='bg-[#214062] h-32 w-32 flex justify-center items-center rounded-full shadow-2xl'>
-              <img src={tailwindLogo} className='size-20'/>
+            <MagneticIcon>
+              <div className='bg-[#214062] h-32 w-32 flex justify-center items-center rounded-full shadow-2xl' data-aos='zoom-in' data-aos-duration="1000" data-aos-once="true" data-aos-delay="1000">
+                <img src={tailwindLogo} className='size-20'/>
+              </div>
             </MagneticIcon>
-            <MagneticIcon className='bg-[#214062] h-28 w-28 flex justify-center items-center rounded-full shadow-2xl'>
-              <img src={viteLogo} className='size-14'/>
+            <MagneticIcon>
+              <div className='bg-[#214062] h-28 w-28 flex justify-center items-center rounded-full shadow-2xl' data-aos='zoom-in' data-aos-duration="1000" data-aos-once="true" data-aos-delay="1200">
+                <img src={viteLogo} className='size-14'/>
+              </div>
             </MagneticIcon>
         </div>
     </div>
